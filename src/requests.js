52b1e84320acd9585a45d82e6e42010bfaf70ea2
Adarsh <adarshgupta7103@gmail.com>
2022-04-12 12:55:57 +0530

    Innitial commit

---
+++ src/requests.js
@@ -0,0 +1,14 @@
+const API_KEY = "d8750c87b214238d4162c4b00b9d4790";
+
+const requests = {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumantaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+};
+
+export default requests;
